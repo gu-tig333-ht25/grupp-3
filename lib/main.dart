391baee9276d5a68c4e42f626b428a8c2605be13
Julia Yngwe Hobson <17475565+Juuliiaa@users.noreply.github.com>
2025-10-20 22:14:27 +0200

    La till nyheterna på hem

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:template/providers/news_provider.dart';
 import 'package:template/screens/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
   runApp(MyApp());
 }
 
@@ -10,14 +16,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bostadskollen',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NewsProvider()..loadNews()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bostadskollen',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
